a7f5474be403ecc3ef4bc6c084265a8876aada89
GitMonies <jarod.smelosky@yahoo.com>
2023-09-18 23:26:22 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+// function saySomething(message = 'goodbye', whisper = false) {
+//     if (whisper) {
+//         console.log(`%c${message}`, 'font-size: 5px');
+//     } else {
+//         console.log(message);
+//     }
+// }
+
+// saySomething('hello', true)
+// saySomething('Goodbye', false)
+
+
+
+
+
+
+// function numberGrads(congratulations) {
+// num1 = prompt('Enter a number!');
+// alert(congratulations + " " + num1 + " dollars!")
+// }
+
+// numberGrads('You have won')
+
+
+
+
+// Accessing objects and methods
+
+// let hero = {
+//     name : 'Achilles',
+//     alias : 'Pizza man',
+//     catchphrase: 'Pepperoni or sausage?!',
+//     speak: function() {
+//         return 'Attention!' + this.catchphrase;
+//     },
+//     attack: function(sound) {
+//         return `(throws out pizzas) ${sound}`;
+//     }
+// };
+
+// let whatHeSay = 'catchphrase'
+
+// console.log(hero[whatHeSay])
+// console.log(hero.alias)
+
+// console.log(hero.speak())
+// console.log(hero.attack('WHOOOSSSHH'))
+
+// let harvest = {
+//     plantType : 'Wheat',
+//     season : 'summer',
+//     climate : 'fair',
+//     reap: function(action) {
+//         return `I shall ${action} the field!`
+//     }
+
+//     }
+
+//     console.log(harvest.reap('harvest'))
+//     console.log(harvest.season)
+
+
+
+
+
+
